86b04ec8df8b507c6f788464e09e70abbe405cff
TayF <taynaya.f@gmail.com>
2019-05-29 11:38:02 -0400

    Refactoring First Pass
    Re-organized the foler structure, modified the loadgame script and some minor changes.

---
+++ TurnBaseHockey/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGame : MonoBehaviour
+{ 
+    public void Load(string toLoad)
+    {
+        SceneManager.LoadScene(toLoad);
+    }
+}
